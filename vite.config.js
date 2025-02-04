88484b2a12041217a077bec7b68627f9efa95eff
mkGerber <michaelgerber2004@gmail.com>
2025-02-03 19:45:42 -0500

    test site

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mkGerber-port/",
+  //base: "/mkGerber-port/",
+  assetsInclude: ["**/*.MOV"], // Include `.MOV` files in the build
 });
